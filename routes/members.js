b75fbdf855a247ef2fdc14f8e8bbd629ac66b28f
Morgoht <amauryandrade1998@gmail.com>
2023-09-01 16:45:20 +0200

    feat(lint/prettier/commitizen): add good pratices modules (lint, prettier,commitizen, jest)

--- routes/members.js
+++ routes/members.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/User.js');
+const User = require("../models/User.js");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
     console.log("MEMBER INDEX");
     console.log("session variable : " + JSON.stringify(req.session));
     if (req.session.login) {
         const user = User.find(req.session.login);
-        res.render('members/index', { user });
-    }
-    else {
-        res.redirect('/users');
+        res.render("members/index", { user });
+    } else {
+        res.redirect("/users");
     }
 });
 
