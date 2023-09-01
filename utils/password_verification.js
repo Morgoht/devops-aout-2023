f30d6a4867a08ee74de24977ac5ced4cf55cb509
Morgoht <amauryandrade1998@gmail.com>
2023-09-01 17:36:10 +0200

    code:fix

--- utils/password_verification.js
+++ utils/password_verification.js
@@ -1,23 +1,19 @@
-
 module.exports = function (password) {
-    if(password.length < 8 ){
+    if (password.length < 8) {
         return false;
-    } 
+    }
 
     const specialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
-    if (!
-        specialChar.test(password)) {
+    if (!specialChar.test(password)) {
         return false;
-      }
-    
-      const contientLettre = /[a-zA-Z]/;
-      const contientChiffre = /[0-9]/;
-      if (!contientLettre.test(password) || !contientChiffre.test(password)) {
+    }
+
+    const contientLettre = /[a-zA-Z]/;
+    const contientChiffre = /[0-9]/;
+    if (!contientLettre.test(password) || !contientChiffre.test(password)) {
         return false;
-      }
-    
-      // Si toutes les conditions sont remplies, le mot de passe est valide
-      return true;
-    
-    
-}
+    }
+
+    // Si toutes les conditions sont remplies, le mot de passe est valide
+    return true;
+};
