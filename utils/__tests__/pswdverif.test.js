662731a5210d5867e50b47320e5f6179838dce11
Morgoht <amauryandrade1998@gmail.com>
2023-09-01 17:17:38 +0200

    fix(add test): add all test on tdd pmethod verif password

--- utils/__tests__/pswdverif.test.js
+++ utils/__tests__/pswdverif.test.js
@@ -3,6 +3,18 @@ const passwordVerification = require ('../password_verification')
 describe('test for password verification', () => {
     
     it("should return false has not contain letter or number", () => {
+        const resultPassword = passwordVerification("1234567")
+        expect(resultPassword).toBe(false);
+    });
+    it("should return false has not contain letter or number", () => {
+        const resultPassword = passwordVerification("12345678@")
+        expect(resultPassword).toBe(false);
+    });
+    it("should return false has not contain letter or number", () => {
+        const resultPassword = passwordVerification("azerivjec@a")
+        expect(resultPassword).toBe(false);
+    });
+    it("should return false has not contain letter or number", () => {
         const resultPassword = passwordVerification("12345678@a")
         expect(resultPassword).toBe(true);
     });
