24d72cef615dd69978e66fd3942f285a1eda34ae
Morgoht <amauryandrade1998@gmail.com>
2023-09-01 17:13:01 +0200

    add cpecial char test

--- utils/__tests__/pswdverif.test.js
+++ utils/__tests__/pswdverif.test.js
@@ -1,14 +1,7 @@
 const passwordVerification = require ('../password_verification')
 
 describe('test for password verification', () => {
-    it("should return false as the password is not contain 8 char", () => {
-        const resultPassword =  passwordVerification("1234567")
-        expect(resultPassword).toBe(false)
-    });
-    it("should return true as the password contain 8 char", () => {
-        const resultPassword =  passwordVerification("12345678")
-        expect(resultPassword).toBe(true)
-    });
+    
     it("should return false has not contain special char", () => {
         const resultPassword = passwordVerification("12345678")
         expect(resultPassword).toBe(false);
